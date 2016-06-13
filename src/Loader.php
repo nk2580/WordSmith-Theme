bba29b676563e2b8b38a112bf1a065882e015353
nik kyriakidis <nik@pushworth.com>
2016-06-13 14:23:29 +1000

    Signed-off-by: nik kyriakidis <nik@pushworth.com>

---
+++ src/Loader.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace nk2580\wordsmith\theme;
+
+/**
+ * Description of Loader
+ *
+ * @author Nik Kyriakidis
+ */
+class Loader {
+    //put your code here
+
+    /*
+     * test for the framework plugin
+     * load your environemt variables
+     * init the theme in wordpress
+     */
+    
+}
